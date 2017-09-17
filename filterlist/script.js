c0838273d8494ad28aa603bdec7152dee99ce220
Mitul Shah <mitulxshah@gmail.com>
2017-09-17 16:02:07 -0400

    filtering complete 🔥

--- filterlist/script.js
+++ filterlist/script.js
@@ -7,4 +7,20 @@ function filterNames(){
 	// get value
 	let filterValue = document.getElementById('filter').value.toUpperCase();
 	
+	// get names Ul
+	let ul = document.getElementById('name');
+	// get li's from ul
+	let li = ul.querySelectorAll('li.collection-item');
+	
+	// loop thru collection item li
+	for(let i = 0; i < li.length; i++){
+		let a = li[i].getElementsByTagName('a')[0];
+		// if it matches correct
+		if(a.innerHTML.toUpperCase().indexOf(filterValue) > -1){
+			li[i].style.display = '';
+		} else {
+			li[i].style.display = 'none';
+		}
+	}
+	
 }
