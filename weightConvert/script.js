75395e70b49f46a5a950055a8cd56ed72072b6d0
Mitul Shah <mitulxshah@gmail.com>
2017-09-17 22:54:21 -0400

    project complete in ten minutes! 🏄‍♂️

--- weightConvert/script.js
+++ weightConvert/script.js
@@ -1,1 +1,7 @@
-console.log('connected');
+var filter = document.getElementById('lbsInput');
+filter.addEventListener('input', weightFilter);
+
+function weightFilter(e){
+	let pounds = e.target.value;	document.getElementById('gramsOutput').innerHTML = pounds/0.0022046;
+	document.getElementById('kgOutput').innerHTML = pounds/2.2046;	document.getElementById('ouncesOutput').innerHTML = pounds * 16;
+}
